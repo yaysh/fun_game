d551879a0dffe385189b92de30be5728bdd72e7d
Jens Madsen <madswed@hotmail.com>
2020-06-04 00:11:30 +0200

    Game entity translated

--- ts/gameobjects/game_entity.ts
+++ ts/gameobjects/game_entity.ts
@@ -1,6 +1,13 @@
 class GameEntity extends GameObject {
     
-    constructor(x, y, vx, vy, width, height, tile) {
+    vx: number;
+    vy: number;
+    width: number;
+    height: number;
+    tile: number;
+
+
+    constructor(x: number, y: number, vx: number, vy: number, width: number, height: number, tile: number) {
         super(x, y);
         this.vx = vx;
         this.vy = vy;
@@ -10,14 +17,14 @@ class GameEntity extends GameObject {
     }
 
     // TODO: make these abstract
-    draw(ctx) {      
+    draw(ctx: CanvasRenderingContext2D) {      
         ctx.beginPath();
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.stroke();
     }
     
     // TODO: make these abstract
-    update(progress) {
+    update(progress: number) {
         const p = progress / 16;
         this.x += this.vx;
         this.y += this.vy;
